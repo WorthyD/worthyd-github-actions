131763e2aabc60b5079944a6dfc8891be2c88775
worthyd <danieljworthy@gmail.com>
2023-02-23 14:33:57 -0600

    Updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 import { secretKeys } from './secrets';
+const packageJson = require('../../package.json');
 export const environment = {
   production: true,
   apiKey: secretKeys.apiKey,
+  version:packageJson.version
 };
